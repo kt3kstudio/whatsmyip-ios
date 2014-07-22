9f87a9c3e85db637604df392d9cecf65ec04ad12
Yoshiya Hinosawa <stibium121@gmail.com>
2014-07-22 22:54:28 +0100

    Initial Commit

---
+++ whatsmyip/KT3AppDelegate.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+@interface KT3AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ whatsmyip/main.m
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "KT3AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KT3AppDelegate class]));
+    }
+}
