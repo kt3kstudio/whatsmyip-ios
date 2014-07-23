7300d025b0ce939dfd16f1bcb6a9aafd97fa495c
Yoshiya Hinosawa <stibium121@gmail.com>
2014-07-23 16:59:23 +0100

    Introduce middleman

---
+++ config.rb
@@ -0,0 +1,21 @@
+# middleman config
+
+set :source, 'm'
+
+set :build_dir, 'whatsmyip/www'
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+configure :build do
+
+  activate :minify_css
+
+  activate :minify_javascript
+
+  activate :relative_assets
+
+end
