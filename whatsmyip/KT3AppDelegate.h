0faa6083a345bffed77f437d58c021f655eadc5e
Yoshiya Hinosawa <stibium121@gmail.com>
2014-07-24 17:10:49 +0100

    Use STWViewController

--- whatsmyip/KT3AppDelegate.h
+++ whatsmyip/KT3AppDelegate.h
@@ -1,7 +1,10 @@
 #import <UIKit/UIKit.h>
+#import <Straw/Straw.h>
+#import <StrawServiceHttp/STWServiceHttp.h>
 
 @interface KT3AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (nonatomic) STWViewController *viewController;
 
 @end
