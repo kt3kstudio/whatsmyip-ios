58bb30c489bb2b75b6be4aad043eecd2cda5eca5
Yoshiya Hinosawa <stibium121@gmail.com>
2014-07-25 17:26:41 +0100

    Use STWAppDelegate

--- whatsmyip/KT3AppDelegate.h
+++ whatsmyip/KT3AppDelegate.h
@@ -1,10 +1,6 @@
-#import <UIKit/UIKit.h>
 #import <Straw/Straw.h>
 #import <StrawServiceHttp/STWServiceHttp.h>
 
-@interface KT3AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-@property (nonatomic) STWViewController *viewController;
+@interface KT3AppDelegate : STWAppDelegate
 
 @end
--- whatsmyip/KT3AppDelegate.m
+++ whatsmyip/KT3AppDelegate.m
@@ -2,27 +2,9 @@
 
 @implementation KT3AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+- (NSArray *)strawServices
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-
-    self.viewController = [[STWViewController alloc] init];
-
-    // load http service
-    [self.viewController loadService:[STWServiceHttp class]];
-
-    // set as the root controller
-    self.window.rootViewController = self.viewController;
-
-    [self.window makeKeyAndVisible];
-
-    return YES;
+    return @[[STWServiceHttp class]];
 }
-
-- (void)applicationWillResignActive:(UIApplication *)application {}
-- (void)applicationDidEnterBackground:(UIApplication *)application {}
-- (void)applicationWillEnterForeground:(UIApplication *)application {}
-- (void)applicationDidBecomeActive:(UIApplication *)application {}
-- (void)applicationWillTerminate:(UIApplication *)application {}
 
 @end
