ae24b2f80d841f67e9bf652aff7a9d88fe51e1f2
Yoshiya Hinosawa <stibium121@gmail.com>
2014-09-28 17:01:56 +0100

    Implement infrastructure.storage

---
+++ infrastructure/storage.js
@@ -0,0 +1,30 @@
+
+window.infrastructure = window.infrastructure || {};
+
+window.infrastructure.storage = (function (Promise, localStorage) {
+    'use strict';
+
+    var exports = {};
+
+    exports.get = function (key, defaultValue) {
+
+        var value = localStorage[key];
+
+        if (value == null) {
+            return Promise.resolve(defaultValue);
+        }
+
+        return Promise.resolve(JSON.parse(value));
+    };
+
+    exports.set = function (key, value) {
+
+        localStorage[key] = JSON.strigify(value);
+
+        return Promise.resolve(true);
+
+    };
+
+    return exports;
+
+}(window.Promise, window.localStorage));
